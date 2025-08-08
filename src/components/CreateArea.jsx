35f12a8613a1e3965f750c5fa519389f3ce78988
ritodevops2003 <ritodevops@gmail.com>
2025-08-08 18:23:25 +0530

    Add files via upload

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import Zoom from "@mui/material/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  const [isExpanded, setExpanded] = useState(false)
+
+  function expand(){
+    setExpanded(true)
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isExpanded ? <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />: null}
+        
+        <textarea
+          name= "content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+          onClick={expand}
+        />
+        <Zoom in={isExpanded ? true : false}>
+        <Fab onClick={submitNote}>
+          <AddIcon />
+        </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+//   return (
+//     <div>
+//       <form className="create-note">
+//         <input
+//           name="title"
+//           onChange={handleChange}
+//           value={note.title}
+//           placeholder="Title"
+//         />
+//         <textarea
+//           name="content"
+//           onChange={handleChange}
+//           value={note.content}
+//           placeholder="Take a note..."
+//           rows="3"
+//         />
+//         <Zoom in={true}>
+//         <Fab onClick={submitNote}>
+//           <AddIcon />
+//         </Fab>
+//         </Zoom>
+//       </form>
+//     </div>
+//   );
+// }
+
+export default CreateArea;
